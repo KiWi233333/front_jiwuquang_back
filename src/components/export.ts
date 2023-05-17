2400ae87caae6977c8a3ccf2b6ede2c705fa6ea2
Kiwi2333 <1329634286@qq.com>
2023-05-18 02:05:31 +0800

    初始化

--- src/components/export.ts
+++ src/components/export.ts
@@ -1,7 +1,7 @@
 import type { App } from "vue";
 
 const modulesFiles = import.meta.glob("./src/components/other/*.vue");
-
+// 批量注册 全局组件
 export default (app:App<Element>) => {
   for (const path in modulesFiles) {
     const componentName:string = modulesFiles[path].name; // 获取组件默认名称
